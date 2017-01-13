166d4e1a0987e9bdc73ed0cc5cc57dcdbe0cbc0d
gregory kelleher <gregory@gregorykelleher.com>
2017-01-13 22:11:13 +0000

    pulling from SQLite

--- app/src/main/java/com/example/gregory/cs385/gamePage.java
+++ app/src/main/java/com/example/gregory/cs385/gamePage.java
@@ -1,11 +1,13 @@
 package com.example.gregory.cs385;
 
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
@@ -14,10 +16,23 @@ import android.widget.Toast;
 
 public class gamePage extends AppCompatActivity {
 
+    DatabaseHelper db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_page);
+        db = new DatabaseHelper(this);
+
+        Button choice_1 = (Button)findViewById(R.id.choice_1);
+
+        Cursor res = db.getAllData();
+
+        StringBuffer buffer  = new StringBuffer();
+        while (res.moveToNext()) {
+            choice_1.setText(res.getString(0));
+        }
+
 
         // change menu colour
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#FFB6C1"));
